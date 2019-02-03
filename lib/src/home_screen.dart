0a672761ec9ece15f5e14814fc8957af7cc3d12e
Hafidh Satyanto <hafidhsatyanto@gmail.com>
2019-02-03 11:54:29 -0600

    initial organizations

---
+++ lib/src/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:fluro/fluro.dart';
+//import './widgets/dissolved_oxygen_gauge.dart';
+//import './widgets/gauge_indicator.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: Center(
+        //child: GaugeIndicator(),
+      )
+    );
+  }
+}
+
