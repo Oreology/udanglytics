f24d84faea49fb306816782a964a64929da3819a
Hafidh Satyanto <hafidhsatyanto@gmail.com>
2019-02-03 12:18:03 -0600

    changed homescreen to stateful widget

--- lib/src/home_screen.dart
+++ lib/src/home_screen.dart
@@ -2,17 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
 import './widgets/gauge_indicator.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+  @override
+  _HomeScreen createState() => _HomeScreen();
+}
+
+class _HomeScreen extends State<HomeScreen> {
+  double val;
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: Center(
-        child: GaugeIndicator(
-          size: 300,
-          inputValue: 100.0,
-        ),
-      )
+        child: Column(children: <Widget>[
+          GaugeIndicator(inputValue: val, size: 300),
+          Slider(value: val,
+          min: 0.0,
+          max: 1.0,
+          onChanged: (sliderInput) {
+            setState(() {
+              val = sliderInput;
+            });
+          }
+          ),
+        ])
+    )
     );
   }
 }
-
